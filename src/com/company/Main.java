29ce9091856d225da0c31518b0d3811dc1603bbf
HIPHOPSANDY <santhoshkumar24cs@gmail.com>
2021-02-16 02:16:12 +0530

    java first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+        float myValue = 50f /10f ;
+        double mySecond = 5d /3d;
+        System.out.println("mySecond = " + mySecond);
+        System.out.println("myValue = " + myValue);
+        double numPounds =200d;
+        double convertedKilograms = numPounds * 0.45359237d;
+        System.out.println("convertedKilograms = " + convertedKilograms);
+        double pi =3.1415927d;
+        System.out.println("pi = " + pi);
+    }
+}
